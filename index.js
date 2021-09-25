1b2b95d008d49bd038b7a196e6bf9d8d991ab18b
info <info@rivitech.co>
2021-09-25 19:39:32 +0530

    node modules deleted

--- index.js
+++ index.js
@@ -1,25 +1,19 @@
 const express = require("express");
 const cors = require("cors");
-const bodyparser = require("body-parser");
-
+const cookieParser = require("cookie-parser");
+const morgan = require("morgan");
 const app = express();
+const helmet = require("helmet");
+
 // app.use(cors(corsOptions));
+app.use(express.json());
+app.use(cookieParser());
+app.use(morgan("combined"));
 app.use(cors());
-app.use(bodyparser.json());
-app.use(bodyparser.urlencoded({ extended: true }));
+app.use(helmet());
 
-// simple route
-app.get("/", (req, res) => {
-  res.send("Welcome to nodejs application!");
-});
 
-// set port, listen for requests
-const PORT = process.env.PORT || 9000;
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}.`);
-});
-
-// creatin tables
+// creating tables
 const sequelize = require('./config/db');
 const User = require('./models/user_table');
 console.log(User);
@@ -31,5 +25,20 @@ const user_route = require("./route/user_route");
 app.use("/user_details", user_route);
 
 // const route_item_purchase = require("./routes/route_item_purchase");
-
 // app.use("/item_purchase", route_item_purchase);
+
+
+
+
+// simple route
+app.get("/", (req, res) => {
+  res.send("Welcome to nodejs application!");
+});
+
+// set port, listen for requests
+
+const PORT = process.env.PORT || 9000;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}.`);
+});
+
